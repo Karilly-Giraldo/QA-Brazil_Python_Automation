c2f181be989d27f93b51ee0754f029762c20d924
Karilly Giraldo <karilly_aha@hotmail.com>
2025-08-11 18:41:50 -0300

    revisão projeto sprint 7

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Arquivo com constantes
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-6041cafd-10c4-4169-9295-fb2e631fa606.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 	'East 2nd Street, 601'
 ADDRESS_TO = 	'1300 1st St'
 PHONE_NUMBER = 	'+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,2 +1,53 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
+    def test_set_route(self):
+        # Adicionar em S8
+        print("Verifica preenchimento correto dos campos de endereço.")
+        assert True  # Placeholder para futura lógica
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("Função criada para definir a seleção de plano.")
+        assert True
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("Função criada para definir o número de telefone.")
+        assert True
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("Função criada para definir o número do cartão.")
+        assert True
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("Função criada para definir o comentário ao motorista.")
+        assert True
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("Função criada para definir pedidos de cobertores e lenços.")
+        assert True
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        number_of_ice_creams = 2
+        for count in range(number_of_ice_creams):
+            print(f"Função criada para definir o pedido de sorvete número {count + 1}.")
+        assert True
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("Função criada para verificar os modelos de carro que aparecem.")
+        assert True
